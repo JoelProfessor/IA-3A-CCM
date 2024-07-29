26aed9e0d0a8ae2a9e351785b42e721259a61191
JoelProfessor <143506257+JoelProfessor@users.noreply.github.com>
2024-07-29 14:28:02 +0000

    .

---
+++ main.js
@@ -0,0 +1,15 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "Quem discubriu o Blasil?",
+        alternativas: [
+            {
+                texto: "Borsonaro",
+                afirmacao: "Não foi o Borso que discubriu o Blasiu. "
+            },
+            {
+                texto: "Pedrinho Arvro Cabrau",
+                afirmacao: "Parabé, você acertou que o Pedrinho discubriu o Blasiu. "
+            }
+        ]
+    }
+];
